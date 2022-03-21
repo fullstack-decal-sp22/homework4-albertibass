e94f8d7a0cb4064a17dee68748aab67f3b686e7d
albertibass <78297908+albertibass@users.noreply.github.com>
2022-03-20 23:28:13 -0700

    hw4

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/Board.css';
 import Square from "./Square";
 
 function Board() {
 
-    const status = 'Next player: X';
+    const [xIsNext, setxIsNext] = useState(true);
+    const [boardState, setBoardState] = useState(Array(9).fill(null));
+
+    function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
+    }
+
+    function handleClick(square) {
+      const squares = boardState.slice();
+      if (calculateWinner(squares) || squares[square]) {
+        return;
+      }
+      squares[square] = xIsNext ? "X" : "O";
+      setBoardState(squares);
+      setxIsNext(!xIsNext)
+    }
+    let winner = calculateWinner(boardState);
+    let status = 'Next player: X';
+
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
 
     function renderSquare(i) {
-        return <Square />;
+        return <Square value = {boardState[i]} onClick = {() => handleClick(i)}/>;
     }
 
     return (  
